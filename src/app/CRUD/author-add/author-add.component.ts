9cfe366991fe9ce57c503add6db446341c7ad07b
Margaux Kowalczyk <m.kowalczyk.pro@gmail.com>
2024-07-15 11:15:37 +0200

    ajout book avec son auteur fonctionnel

---
+++ src/app/CRUD/author-add/author-add.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthorService } from '../../services/author.service';
+
+@Component({
+  selector: 'app-author-add',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './author-add.component.html',
+  styleUrl: './author-add.component.css'
+})
+export class AuthorAddComponent {
+  authorForm: FormGroup;
+
+  @Output() authorAdded = new EventEmitter<number>();
+
+  constructor(
+    private fb: FormBuilder,
+    private authorService: AuthorService
+  ) {
+    this.authorForm = this.fb.group({
+      LastName: [''],
+      FirstName: [''],
+    });
+  }
+
+  addAuthor() {
+    if (this.authorForm.valid) {
+      this.authorService.addAuthor(this.authorForm.value).subscribe(author => {
+        this.authorAdded.emit(author.id);
+      });
+    }
+  }
+}
--- src/app/CRUD/panel-add/panel-add.component.ts
+++ src/app/CRUD/panel-add/panel-add.component.ts
@@ -3,25 +3,32 @@ import { FormGroup, FormControl, ReactiveFormsModule, FormBuilder, Validators } 
 import Book from '../../models/book.model';
 import { BookService } from '../../services/book.service';
 import { RouterLink } from '@angular/router';
+import { AuthorService } from '../../services/author.service';
+import { AuthorAddComponent } from '../author-add/author-add.component';
 
 @Component({
   selector: 'app-panel-add',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, AuthorAddComponent],
   templateUrl: './panel-add.component.html',
   styleUrls: ['./panel-add.component.css']
 })
 export class PanelAddComponent {
-  infoBook: FormGroup = this.formBuilder.group({
-    title: ['titre', Validators.required],
-    // nom_auteur: ['nomA', Validators.required],
-    // prenom_auteur: ['nomP', Validators.required],
-    coverText: ['CT'],
-    comment: ['comm'],
-  });
-  submitted: boolean = false;
+  infoBook: FormGroup;
+  showAuthorAddForm = false;  // Pour afficher/masquer le formulaire d'ajout d'auteur
+  submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private bookService: BookService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private bookService: BookService,
+    private authorService: AuthorService
+  ) {
+    this.infoBook = this.formBuilder.group({
+      title: ['titre', Validators.required],
+      authorId: [null, Validators.required],
+      coverText: ['CT'],
+      comment: ['comm'],
+    });
   }
 
   addBook() {
@@ -29,20 +36,16 @@ export class PanelAddComponent {
     if (this.infoBook.invalid) {
       return;
     }
-    // const newBook: Book = {
-    //   title: this.infoBook.value.titre,
-    //   author: {
-    //     id: 0,
-    //     firstName: this.infoBook.value.prenom_auteur,
-    //     lastName: this.infoBook.value.nom_auteur,
-    //   },
-    //   coverText: this.infoBook.value.coverText,
-    //   comment: this.infoBook.value.comment,
-    // };
     console.log(this.infoBook.value);
 
     this.bookService.addBook(this.infoBook.value).subscribe();
     this.infoBook.reset();
     this.submitted = false;
+  }
+
+  // Méthode pour recevoir l'ID de l'auteur créé
+  onAuthorAdded(authorId: number) {
+    this.infoBook.patchValue({ authorId });
+    this.showAuthorAddForm = false;  // Masquer le formulaire d'ajout d'auteur
   }
 }
